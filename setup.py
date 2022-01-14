f31c6e0e30d3d7d04487fa92b9aa25bd58dc7db1
kk <kkh900922@gmail.com>
2022-01-14 12:51:15 +0000

    this project not support 3.x.x, avoid install wtforms 3.x.x

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '1.0.0-p3',
     packages = find_packages(),
     install_requires = [
+        "wtforms==2.3.3"
         "werkzeug==1.0.1",
         "Flask==1.1.2",
         "Flask-Login==0.5.0",
