cb99b10da53421927ca1247d0f76ff141b42ed8a
kk <kkh900922@gmail.com>
2022-02-02 15:09:38 +0000

    use generator

--- service/repository_licenses.py
+++ service/repository_licenses.py
@@ -2,42 +2,43 @@
 Functions which lists all the licenses associated with a repository account
 Matched on the account's Bibid and the participant list associated with licenses.
 """
+import itertools
+from typing import Iterable
 
 from service import models
 
 
-def get_matching_licenses(account_id):
+def get_matching_licenses(account_id) -> Iterable[dict]:
     account = models.Account.pull(account_id)
-    licenses = []
-    matching_licenses = []
     if not account.has_role('repository'):
-        return licenses
+        return []
+
     # Get repository config for account
     rec = models.RepositoryConfig.pull_by_repo(account_id)
+
     # Get all matching license from alliance (participant) data
     alliances = models.Alliance.pull_by_participant_id(account.repository_bibid)
-    if not alliances:
-        alliances = []
-    for alliance in alliances:
-        license = models.License.pull(alliance.license_id)
-        if not license:
-            continue
-        matching_licenses.append(license)
+    alliances = alliances or []
+    licenses = (models.License.pull(alliance.license_id)
+                for alliance in alliances)
+    licenses = filter(None, licenses)
+
     # Get all gold licences if it isn't a subject repository
+    gold_licences = []
     if not account.has_role('subject_repository'):
         gold_licences = models.License.pull_by_key('type', 'gold')
-        if not gold_licences:
-            gold_licences = []
-        for license in gold_licences:
-            if not license:
-                continue
-            matching_licenses.append(license)
+        gold_licences = filter(None, gold_licences or [])
+
     # prepare list of matching licenses with preferred information
-    for license in matching_licenses:
-        if license.id in [l['id'] for l in licenses]:
+    license_data_list = []
+    for lic in itertools.chain(licenses, gold_licences):
+        if lic.id in [l['id'] for l in license_data_list]:
             continue
-        checked = True
-        if rec and license.id in rec.excluded_license:
-            checked = False
-        licenses += [{"id": license.id, "name": license.name, "type": license.type, "checked": checked}]
-    return licenses
+
+        checked = rec is None or lic.id not in rec.excluded_license
+        license_data_list.append(
+            {"id": lic.id, "name": lic.name, "type": lic.type,
+             "checked": checked}
+        )
+
+    return license_data_list
