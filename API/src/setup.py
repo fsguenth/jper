b797278e81f0e2dcbc07dea2ddeccf702ee077c1
Anusha Ranganathan <anusha@cottagelabs.com>
2023-10-11 09:02:21 +0100

    Bumped version number,to remive hyphen

--- API/src/setup.py
+++ API/src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'engine',
-    version = '1.0.0-p3',
+    version = '1.0.3',
     packages = find_packages(),
     install_requires = [
         "utils"
