dfda320fdeaa924059d71b5be340075afd468d83
Anusha Ranganathan <anusha@cottagelabs.com>
2024-04-20 01:40:06 +0530

    Forgot to add file

---
+++ service/lib/csv_helper.py
@@ -0,0 +1,19 @@
+import chardet
+import io
+
+def _read_uploaded_file(uploaded_file):
+    file_bytes = uploaded_file.stream.read()
+    return file_bytes
+
+def _decode_csv_bytes(csv_bytes):
+    encoding = chardet.detect(csv_bytes)['encoding']
+    encoding_str = 'utf-8'
+    if encoding in ['ISO-8859-1', 'UTF-8-SIG', 'UTF-8', 'utf-8']:
+        encoding_str = encoding
+    else:
+        app.logger.warning(f'unknown encoding[{encoding}], decode as utf8')
+    try:
+        decoded_csv_bytes = csv_bytes.decode(encoding=encoding_str, errors='ignore')
+    except Exception as e:
+        return False, str(e)
+    return True, decoded_csv_bytes
